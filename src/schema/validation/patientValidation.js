0f034d33bb38628ad1793640b6832611842d3642
m.hari16 <active.hari16@gmail.com>
2024-07-10 19:26:11 +0700

    fix: profile patient

--- src/schema/validation/patientValidation.js
+++ src/schema/validation/patientValidation.js
@@ -3,9 +3,11 @@ import Joi from "joi";
 export const createPatient = Joi.object({
   fullName: Joi.string().required(),
   email: Joi.string().email().allow(""),
+  nik: Joi.string().length(16).required(),
   phoneNumber: Joi.string().max(14).allow(""),
   homeAddress: Joi.string().required(),
   gender: Joi.string().valid("M", "F").required(),
+  maritalStatus: Joi.string().valid("SINGLE", "MARRIED", "DIVORCE").required(),
   dateOfBirth: Joi.date().required(),
   bloodGroup: Joi.string().allow(""),
 });
